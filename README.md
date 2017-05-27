edd8018308e4109e1f7f810313670fac70e9179d
Logan <logan.robert.bingham@gmail.com>
2017-05-27 07:41:47 -0400

    cleaned up code a little and added jquery via ajax for github pages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Wee-Sketchy-McPadster
 
-This is my solution for the Odin Project's JavaScript/jQuery project. You can view the assignment sheet [here](https://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery?ref=lnav)
+This is my solution for the Odin Project's JavaScript/jQuery project. You can view the assignment sheet [here](https://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery?ref=lnav) and check out my solution in action [here]().
 
-The goal is to build something like an Etch-A-Sketch: a grid of square divs that change color when you mouse over them, leaving behind a trail for pixelated doodling.
+The goal was to build a tool for pixelated in-browser doodling. I went for a simple, clean look with a few features like rainbow and dot modes and an eraser. Instead of using the prompt method to allow users to set the pixel size, I included a wee popup.
--- application.js
+++ application.js
@@ -1,16 +1,18 @@
 
 var sideLength = 10;
 
-//Get start over figured out and you're good to go
-
 $(document).ready(function() {
 
-  //Popup adapted from flerovium: http://www.jqueryscript.net/lightbox/Simple-jQuery-Plugin-For-Opening-A-Popup-Window-On-Page-load.html
+  //LANDING POPUP adapted from flerovium: http://www.jqueryscript.net/lightbox/Simple-jQuery-Plugin-For-Opening-A-Popup-Window-On-Page-load.html
   var id = '#dialog';
+
   //Get the screen height and width
   var maskHeight = $(document).height();
   var maskWidth = $(window).width();
 
+  $(popUp);
+
+  function popUp() {
   //Set height & width to mask the whole screen
   $('#mask').css({'width': maskWidth, 'height': maskHeight});
 
@@ -29,7 +31,7 @@ $(document).ready(function() {
   //transition effect
   $(id).fadeIn(2000);
 
-  //Form submits on keypress Enter
+  //Submit on enter
   $('input[name=quantity]').keypress(function(e) {
     if(e.which == 13) {
       if($('input[name=quantity]').focus()) {
@@ -48,46 +50,20 @@ $(document).ready(function() {
       $(genGrid);
       $(blackDraw);
     } else {
-      $('#popuptitle').text('Cheeky bastard');
-      $('#popuptext').text('Enter a number smaller than 100, please. I don\'t want to freeze up your browser.')
+      $(cheekyBastard);
     }
   });
+}
 
-//BUILDING BUTTONS AND CONTROLS
+//BUTTONS AND CONTROLS
 
   //Start over
   $('#start').click(function() {
     $('#popuptitle').text('Starting over?');
     $('#popuptext').text('How many pixels per side would you like (max 100)?')
     $('#grid').empty();
-    //Repeating the popup steps above; might be good to refactor?
-    $('#mask').css({'width': maskWidth, 'height': maskHeight});
-    $('#mask').fadeIn(500);
-    $('#mask').fadeTo("slow", 0.9);
-    $(id).css('top', winH/2-$(id).height()/2);
-    $(id).css('left', winW/2-$(id).width()/2);
-    $(id).fadeIn(2000);
-    $('input[type=submit]').click(function() {
-      sideLength = $('input[name=quantity]').val();
-      $('#grid').empty();
-      if (sideLength > 0 && sideLength <= 100) {
-        $('#mask').fadeOut(500);
-        $('.window').fadeOut(500);
-        $('#rainbow, #eraser, #dot').removeClass('highlighted');
-        $(genGrid);
-        $(blackDraw);
-      } else {
-        $('#popuptitle').text('Cheeky bastard');
-        $('#popuptext').text('Between 1 and 100, please.')
-      }
-    });
-    $('input[name=quantity]').keypress(function(e) {
-      if(e.which == 13) {
-        if($('input[name=quantity]').focus()) {
-          $('input[type=submit]').click();
-        }
-      }
-    });
+    $('#rainbow, #eraser, #dot').removeClass('highlighted');
+    $(popUp);
   });
 
   //Turn on eraser
@@ -117,11 +93,13 @@ $(document).ready(function() {
       $(blackDraw);
     } else {
       $(this).addClass('highlighted');
-      $(rainbow)};
+      $(rainbow);
+    };
   });
+
 });
 
-
+//Major functions
 
 function genGrid() {
   //Set grid size and generate
@@ -154,137 +132,7 @@ function rainbow() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-//Below is anyafink's solution which I don't love. am setting aside for now
-//for my flawed genGrid, which I'll fix later i spose
-/*
-    for(var i = 0; i < sideLength; i++) {
-      var $row = $('<div class="row"></div>');
-      $('#grid').append($row.clone());
-    }
-
-    for(var i = 0; i < sideLength; i++) {
-      var $square = $('<div class="square"></div>');
-      $('.row').append($square.clone());
-    }
-
-    var calc = 100/sideLength;
-    calc -= calc * .01;
-
-    $('.row').css({'height': calc + '%'});
-    $('.square').css({'width': calc + '%'});
-  }
-});
-
-*/
-//Note: add to the above to make this work when you push Enter too
-
-/*
-  //If close button is clicked
-  $('.window .close').click(function (e) {
-    //Cancel the link behavior
-    e.preventDefault();
-    $('#mask').hide();
-    $('.window').hide();
-  });
-
-
-//if mask is clicked
-$('#mask').click(function() {
-  $(this).hide();
-  $('.window').hide();
-});
-*/
-
-
-
-
-  //for (var i=0; i<sideLength.length; i++) {
-//Let's get started! Set initial grid size like https://beachfern.github.io/sketchpad/
-
-/* First attempt at making the div grid
-
-
-//The following solution works, but it takes for fucking ever to load
-$(document).ready(function() {
-  for (var i = 0; i < (sideLength*sideLength); i++) {
-    $("#grid").append($('<div class="divGrid"></div>'));
-    $('.divGrid').css('height', divDimension + 'px');
-    $('.divGrid').css('width', divDimension + 'px');
-
-    I'm gonna try to pull the jQuery selectors out of the loop to speed it up
-
-$('.divGrid').height(divDimension).width(divDimension);
-
-$(document).ready(function() {
-  $('#divGrid').height(divDimension).width(divDimension);
-    for (var i = 0; i < (divDimension*sideLength); i++) {
-      $('#divGrid').clone().appendTo('#grid');
-    }
-});
-
-first attempt at copying:
-
-function genGrid(sideLength) {
-  for (i = 0, i < sideLength * sideLength; i ++) {
-    $('<div>', {class: 'divDimension'}).appendTo($('#grid'));
-  }
-  makeSquare()
+function cheekyBastard() {
+  $('#popuptitle').text('I see what you did there.');
+  $('#popuptext').text('100 or less, please, you cheeky bastard. I don\'t want to freeze up your browser.')
 }
-
-function makeSquare() {
-  square = 400 / sideLength
-  $('.divDimension').css({'height', square + 'px'}, {'width', square + 'px'});
-}
-
-  }
-
-*/
-
-/*
-function drawGrid() {
-
-
-
-  //Draws grid based on user input
-
-
-};
-
-function colorChange(){
-
-  //change div color on mouseenter, maybe using addClass() or background-color
-  //Also you'll definitely want to use $(this)
-}
-
-function(){
-
-  //Darken on second pass like https://anyafink.github.io/sketchpad/
-}
-
-function() {
-
-//update sideLength from form input when user clicks submit
-
-}
-
-function(){
-  //RGB color selector like https://ridergit.github.io/Sketchpad/
-
-}
-
-function(){
-
-  //eraser
-}
-*/
